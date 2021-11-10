2e41d9ca4bd85532ca4e31185737f8ce886b1388
razon1494 <razon1494@gmail.com>
2021-11-11 00:49:28 +0600

    server updated

--- index.js
+++ index.js
@@ -84,6 +84,18 @@ app.get('/reviews', async (req, res) => {
       const review=req.body;
       const result=await reviewCollection.insertOne(review);
       res.json(result);
+      })
+    //finding admin
+    //find admin
+    app.get('/users/:email', async (req, res) => {
+      const email=req.params.email;
+      const query={email: email};
+      const user=await usersCollection.findOne(query);
+      let isAdmin=false;
+      if(user?.role==='admin') {
+        isAdmin=true;
+      }
+      res.json({admin: isAdmin});
     })
 
     //add new product
